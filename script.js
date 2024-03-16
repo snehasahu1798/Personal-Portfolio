910aeafc8c847811686bf99e2df3fbe91ed60641
snehasahu1798 <127763407+snehasahu1798@users.noreply.github.com>
2024-03-16 00:23:49 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+
+// color: rgb(53, 53, 53); 
+//   font-family: 'Rouge Script', cursive; 
+//   font-size: 60px; 
+//   font-weight: normal; 
+//   line-height: 48px;
+//    margin: 40 0 50px; 
+//    text-align: center;
+//     text-shadow: 1px 2px 2px #000102;
